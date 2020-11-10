816a4ffcefe2e94703f9346377302b285a25d00b
Cliona Hayden <c00225401@itcarlow.ie>
2020-11-10 14:10:23 +0000

    Created base project

---
+++ EnemyBehaviour/Assets/Scripts/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Vector3 newPos;
+
+    // moves player to mouse click position for testing purposes 
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPos.z = transform.position.z;
+            transform.position = newPos;
+        }
+    }
+}
