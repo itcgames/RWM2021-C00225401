5010ca645041063090e68763be2b9e877d00617a
Cliona Hayden <c00225401@itcarlow.ie>
2020-11-12 16:28:49 +0000

    XG-4 # thime 2h The enemy stop when there is an obstacle in their path complete

--- EnemyBehaviour/Assets/Scripts/Follow.cs
+++ EnemyBehaviour/Assets/Scripts/Follow.cs
@@ -12,13 +12,10 @@ public class Follow : MonoBehaviour
     public float wakeRadius;
     [SerializeField]
     public float speed;
-    [SerializeField]
-    public static float FOV = 360.0f;
+
 
     private bool awake = true;
-    private float angle = 0.0f;
-    private static int raycount = 5;
-    private float angleIncrease = FOV / raycount;
+
 
     void Start()
     {
@@ -27,29 +24,9 @@ public class Follow : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        var dir = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
-        bool seen = false;
-        Vector3 vertex = new Vector3(0,0,0);
-        //RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, dir, wakeRadius);
-        //for (int i = 0; i < raycount; i++)
-        //{
-        //    if (raycastHit.collider.gameObject.name == "Player")
-        //    {
-        //        vertex = raycastHit.point;
-        //        awake = true;
-        //        seen = true;
-        //    }
-        //    Debug.DrawRay(vertex, dir * wakeRadius, Color.green);
-        //}
-        if (!seen)
-        {
-            vertex = transform.position + dir * wakeRadius;
-            //awake = false;
-        }
+    { 
         if (awake)
         {
-            seen = false;
             moveToTarget();
         }
     }
