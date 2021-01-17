31b90f378b6b7429802f84f7eacaf34c27ea04c9
Cliona Hayden <c00225401@itcarlow.ie>
2021-01-17 21:09:09 +0000

    added timer and time to live to puddles

--- EnemyBehaviour/Assets/Scripts/Puddle.cs
+++ EnemyBehaviour/Assets/Scripts/Puddle.cs
@@ -7,10 +7,22 @@ public class Puddle : MonoBehaviour
     [SerializeField]
     private float timeToLive;
 
+    private float timerReset;
+
+    private void Start()
+    {
+        timerReset = timeToLive;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        timeToLive -= Time.deltaTime;
+        if (timeToLive < 0)
+        {
+            timeToLive = timerReset;
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
