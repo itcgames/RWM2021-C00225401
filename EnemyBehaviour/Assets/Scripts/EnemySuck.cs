f92748a99d2baa2fb59bc8c3b5136a54b4d01796
Cliona Hayden <c00225401@itcarlow.ie>
2021-01-17 19:09:12 +0000

    Complated script for enemy behaviour

---
+++ EnemyBehaviour/Assets/Scripts/EnemySuck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySuck : MonoBehaviour
+{
+    [SerializeField]
+    private float suckRadius;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;
+        if ((playerPos - transform.position).magnitude < suckRadius)
+        {
+            Vector3 attraction = (playerPos - transform.position).normalized;
+            player.transform.position = playerPos - (attraction * (speed * Time.deltaTime));
+        }
+
+    }
+}
